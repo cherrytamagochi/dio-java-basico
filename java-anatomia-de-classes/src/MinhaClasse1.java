67d6b3a06cfd754e28b611141ea94c1e5a9ce428
giovanna <giovannadacostalopes@gmail.com>
2024-06-04 13:56:31 -0300

    add boletim

---
+++ java-anatomia-de-classes/src/MinhaClasse1.java
@@ -0,0 +1,7 @@
+public class MinhaClasse1 {
+    
+    public static void main (String [] args) {
+
+        System.out.print ("Olá bem vindo(a)!!!");
+    }
+}
--- java-anatomia-de-classes/src/MinhaClasse2.java
+++ java-anatomia-de-classes/src/MinhaClasse2.java
@@ -1,4 +1,4 @@
-public class MinhaClasse {
+public class MinhaClasse2 {
     
     public static void main(String [] args) {
 
