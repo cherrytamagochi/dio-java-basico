f3ef798cc4864461d0365b416955357603e9507f
giovanna <giovannadacostalopes@gmail.com>
2024-06-14 10:50:18 -0300

    Estudando Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo =2.500;
-
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 =(short) numeroNormal;
 
         final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
