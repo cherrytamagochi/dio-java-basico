40681f1a95f578ee72130c293be5440bae7ddf9f
giovanna <giovannadacostalopes@gmail.com>
2024-06-14 10:21:59 -0300

    estudando variaveis

---
+++ tipos-variaveis/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo =2.500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 =(short) numeroNormal;
+    }
+}
